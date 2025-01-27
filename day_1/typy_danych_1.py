4e3963cf376c238ddcd6a5ee4f192481cf179b55
rjaniak <rajkonkret660@gmail.com>
2025-01-27 14:45:03 +0100

    [1 11] add typy_danych_1.py

---
+++ day_1/typy_danych_1.py
@@ -0,0 +1,48 @@
+import sys
+
+wiek = 47  # int
+rok = 2025  # int
+temp = 36.6  # float
+# 36,6
+print(temp)
+print(type(temp))  # <class 'float'>
+
+print(wiek + rok)  # 2072
+print(wiek - rok)  # -1978
+print(wiek * rok)  # 95175
+print(wiek / rok)  # 0.023209876543209877
+
+print(rok // wiek)  # część całkowita z dzielenia, 43
+print(rok % wiek)  # reszta z dzielenia, reszta 4
+print(5 % 2)  # r1
+
+print(wiek ** rok)  # potęgowanie
+
+# len() sprawdzenie długości
+print(len(str(wiek ** rok)))  # 3386 znaków
+# print(len(str(wiek ** rok ** 2))) #
+# ValueError: Exceeds the limit (4300 digits) for integer string conversion;
+# use sys.set_int_max_str_digits() to increase the limit
+
+print(54 - 5 * 43 + 4 / 2 + 4 / 2)  # -157.0
+print(54 - 5 * 43 + (4 / 2 + 4) / 2)  # -158.0
+
+# liczby typu float
+# ma bład zaokrąglenia
+print(0.2 + 0.8)  # 1.0
+print(0.2 + 0.7)  # 0.8999999999999999
+print(0.1 + 0.2)  # 0.30000000000000004
+# For example, in a floating-point arithmetic with five base-ten digits,
+# the sum 12.345 + 1.0001 = 13.3451 might be rounded to 13.345.
+# decimal - pozwala ominąć bład zaokrąglenia
+print(sys.float_info)
+# sys.float_info(max=1.7976931348623157e+308, max_exp=1024, max_10_exp=308,
+#                min=2.2250738585072014e-308, min_exp=-1021, min_10_exp=-307,
+#                dig=15, mant_dig=53, epsilon=2.220446049250313e-16, radix=2, rounds=1)
+
+print(f"Sprawdzenie zmiennej {temp} {wiek}")  # Sprawdzenie zmiennej 36.6 47
+print(f"""
+{temp}
+    {wiek}""")
+# "36.6
+#     47"
