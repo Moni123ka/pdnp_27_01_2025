ce82cb1fb8e1cab74ea9a21899d0a6dc47f824c5
rjaniak <rajkonkret660@gmail.com>
2025-01-28 10:51:35 +0100

    [2 4] update typy_danych_4_zbior.py

--- day_2/typy_danych_4_zbior.py
+++ day_2/typy_danych_4_zbior.py
@@ -41,3 +41,36 @@ print(zbior_copy)  # {18, 22, 24, 11, 44}
 print(zbior)  # {11, 44, 18, 22, 24}
 print(id(zbior))  # 2749828112544
 print(id(zbior_copy))  # 2749828108064
+
+# operaacje na zbiorach
+zbior_2 = {667, 11, 44, 12.34, 18, 52, 667, 62}
+print(type(zbior_2))  # <class 'set'>
+print(zbior_2)  # {18, 667, 52, 11, 44, 12.34, 62}
+
+# suma zbiorów - zwraca nowy zbiór
+print(zbior | zbior_2)  # {11, 44, 12.34, 18, 52, 22, 24, 667, 62}
+print(zbior.union(zbior_2))  # {11, 44, 12.34, 18, 52, 22, 24, 667, 62}
+
+# cczęśc wspólna
+print(zbior & zbior_2)  # {18, 11, 44}
+print(zbior.intersection(zbior_2))  # {18, 11, 44}
+
+# różnica
+print(zbior - zbior_2)  # {24, 22}
+print(zbior.difference(zbior_2))  # {24, 22}
+print(zbior_2.difference(zbior))  # {667, 52, 12.34, 62}
+
+# łaczy zbiory, zmmienia bazowy!!!
+zbior.update(zbior_2)
+print(zbior)  # {11, 44, 12.34, 18, 52, 22, 24, 667, 62}
+
+krotka = tuple(zbior)
+print(krotka)  # (11, 44, 12.34, 18, 52, 22, 24, 667, 62)
+
+lista = list(zbior)
+print(lista)  # [11, 44, 12.34, 18, 52, 22, 24, 667, 62]
+
+# sprawdzamy czy dany element istnieje w kolekcji
+print(667 in zbior)  # True
+print(777 in lista)  # False
+print(777 in krotka)  # False
