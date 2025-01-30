937a6d40f5877d6738990b2dd24e7dfd321a9e79
rjaniak <rajkonkret660@gmail.com>
2025-01-30 11:29:16 +0100

    [4 6] update fun_6.py

--- day_4/fun_6.py
+++ day_4/fun_6.py
@@ -1,18 +1,21 @@
 # stworzyc funkcję obliczającą średnią
 
-def liczby(*cyfry):  # dowolna ilosć argumentów pozycyjnych
+def liczby(name=None, *cyfry):  # dowolna ilosć argumentów pozycyjnych
     print(cyfry)
     count = len(cyfry)
     suma = 0
+    suma_p = sum(cyfry)
     try:
         for c in cyfry:
             suma += c
 
         avg = suma / count
+        avg_p = suma_p / count
     except Exception as e:
         print("Bład:", e)
     else:
-        print(f"Średnia wynosi {avg}")
+        print(f"Średnia dla ucznia {name} wynosi {avg}")
+        print(f"Średnia dla ucznia {name} wynosi {avg_p}")
     finally:
         print("Obliczenia zakończone")
 
@@ -24,4 +27,20 @@ liczby(1, 2, 3, 4, 5, 6)  # (1, 2, 3, 4, 5, 6)
 # Obliczenia zakończone
 # (1, 2, 3, 4, 5, 6)
 # Średnia wynosi 3.5
+# Obliczenia zakończone
+# ("Radek", 1,2,3,4,5,6) -> name, *cyfry
+liczby("Radek", 1, 2, 3, 4, 5, 6)
+# (1, 2, 3, 4, 5, 6)
+# Średnia dla ucznia Radek wynosi 3.5
+# Obliczenia zakończone
+# ()
+# Bład: division by zero
+# Obliczenia zakończone
+# (2, 3, 4, 5, 6)
+# Średnia dla ucznia 1 wynosi 4.0
+# Średnia dla ucznia 1 wynosi 4.0
+# Obliczenia zakończone
+# (1, 2, 3, 4, 5, 6)
+# Średnia dla ucznia Radek wynosi 3.5
+# Średnia dla ucznia Radek wynosi 3.5
 # Obliczenia zakończone
